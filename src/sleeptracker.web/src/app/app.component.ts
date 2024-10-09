fd52669d1945f00a029a702e38a49947f805b335
Chris Carter <108958713+chrisjamiecarter@users.noreply.github.com>
2024-10-09 20:29:34 +0100

    Re-bootstrapped with scss instead
    Added toolbar
    
    #5

--- src/sleeptracker.web/src/app/app.component.ts
+++ src/sleeptracker.web/src/app/app.component.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [MatSlideToggleModule, RouterOutlet],
+  imports: [RouterOutlet, MatToolbarModule, MatButtonModule, MatIconModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'Sleep Tracker';
+  title = 'sleeptracker.web';
 }
