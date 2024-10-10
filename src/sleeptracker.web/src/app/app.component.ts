4d5ce705564d50073ec38782703755131158f25f
Chris Carter <108958713+chrisjamiecarter@users.noreply.github.com>
2024-10-10 20:02:08 +0100

    Created main page

--- src/sleeptracker.web/src/app/app.component.ts
+++ src/sleeptracker.web/src/app/app.component.ts
@@ -1,18 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
-import { MatIconModule } from '@angular/material/icon';
-import { MatButtonModule } from '@angular/material/button';
-import { MatToolbarModule } from '@angular/material/toolbar';
-
+import { HeaderComponent } from './header/header.component';
 import { SleepRecordsComponent } from './sleep-records/sleep-records.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatToolbarModule, MatButtonModule, MatIconModule, SleepRecordsComponent],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, SleepRecordsComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
+  
 }
--- src/sleeptracker.web/src/app/app.routes.ts
+++ src/sleeptracker.web/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { SleepRecordsComponent } from './sleep-records/sleep-records.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: SleepRecordsComponent,
+        title: 'Sleep Tracker - Home',
+      },
+];
