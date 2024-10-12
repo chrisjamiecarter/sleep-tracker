1dfd76991d3d1abb6ea10bcccb3cf5be9cd61545
Chris Carter <108958713+chrisjamiecarter@users.noreply.github.com>
2024-10-12 17:54:55 +0100

    Added add sleep record dialog

--- src/sleeptracker.web/src/app/header/header.component.ts
+++ src/sleeptracker.web/src/app/header/header.component.ts
@@ -1,18 +1,32 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
-
+import {
+  MatDialogModule,
+  MatDialog,
+  MAT_DIALOG_DATA,
+  MatDialogTitle,
+  MatDialogContent,
+} from '@angular/material/dialog';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { AddSleepRecordDialogComponent } from '../sleep-records/add-sleep-record-dialog/add-sleep-record-dialog.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [RouterLink, MatToolbarModule, MatButtonModule, MatIconModule],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
+  addSleepRecordRialog = inject(MatDialog);
 
+  openAddSleepRecordDialog() {
+    this.addSleepRecordRialog.open(AddSleepRecordDialogComponent, {
+      height: '30%',
+      width: '30%',
+    });
+  }
 }
