997131769dba66d371eb43e037a8685ff6ab96fa
Chris Carter <108958713+chrisjamiecarter@users.noreply.github.com>
2024-10-14 19:00:28 +0100

    Added snack bars

--- src/sleeptracker.web/src/app/sleep-records/add-sleep-record-dialog/add-sleep-record-dialog.component.ts
+++ src/sleeptracker.web/src/app/sleep-records/add-sleep-record-dialog/add-sleep-record-dialog.component.ts
@@ -1,12 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule, MatDialogTitle, MatDialogContent } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { SleepRecordService } from '../../shared/sleep-record.service';
 import { CreateSleepRecord } from '../../shared/create-sleep-record.interface';
+import { SuccessSnackBarComponent } from '../../snack-bars/success-snack-bar/success-snack-bar.component';
+import { ErrorSnackBarComponent } from '../../snack-bars/error-snack-bar/error-snack-bar.component';
 
 @Component({
   selector: 'app-add-sleep-record-dialog',
@@ -26,6 +29,7 @@ import { CreateSleepRecord } from '../../shared/create-sleep-record.interface';
   styleUrl: './add-sleep-record-dialog.component.scss',
 })
 export class AddSleepRecordDialogComponent implements OnInit {
+  private _snackBar = inject(MatSnackBar);
   sleepRecordForm!: FormGroup;
   
   constructor(private sleepRecordService: SleepRecordService, private formBuilder: FormBuilder) { }
@@ -38,16 +42,32 @@ export class AddSleepRecordDialogComponent implements OnInit {
   }
 
   onCreate() {
-    console.log("sleepRecordForm.value", this.sleepRecordForm.value);
-    console.log("sleepRecordForm.valid", this.sleepRecordForm.valid);
     if (this.sleepRecordForm.valid) {
       const request : CreateSleepRecord = {
         started: this.sleepRecordForm.value.started ?? '',
         finished: this.sleepRecordForm.value.finished ?? '',
       };
       this.sleepRecordService.createSleepRecord(request).subscribe((result) => {
-        console.log("createSleepRecord.result", result);
+        if (result) {
+          this.openSuccessSnackBar('Sleep recorded successfully!');
+        } else {
+          this.openErrorSnackBar('Unable to record Sleep!');
+        }
       });
     }
+  }
+
+  openErrorSnackBar(message: string) {
+    this._snackBar.openFromComponent(ErrorSnackBarComponent, {
+      data: message,
+      duration: 3000,
+    });
+  }
+
+  openSuccessSnackBar(message: string) {
+    this._snackBar.openFromComponent(SuccessSnackBarComponent, {
+      data: message,
+      duration: 3000,
+    });
   }
 }
