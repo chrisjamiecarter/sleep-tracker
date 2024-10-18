3e46e585d5a247a51cccafcb2394c822f1585bc7
Chris Carter <108958713+chrisjamiecarter@users.noreply.github.com>
2024-10-18 22:10:51 +0100

    Initial timer outline implemented

--- src/sleeptracker.web/src/app/sleep-timer/sleep-timer.component.ts
+++ src/sleeptracker.web/src/app/sleep-timer/sleep-timer.component.ts
@@ -1,12 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-sleep-timer',
   standalone: true,
-  imports: [],
+  imports: [MatButtonModule, MatCardModule],
   templateUrl: './sleep-timer.component.html',
-  styleUrl: './sleep-timer.component.scss'
+  styleUrl: './sleep-timer.component.scss',
 })
-export class SleepTimerComponent {
+export class SleepTimerComponent implements OnDestroy {
+  counter: number | undefined;
+  timerRef: any;
+  running: boolean = false;
+  startText = 'Start';
+
+  startTimer() {
+    this.running = true;
+    const startTime = Date.now() - (this.counter || 0);
+    this.timerRef = setInterval(() => {
+      this.counter = Date.now() - startTime;
+    }, 1000);
+  }
+
+  stopTimer() {
+    this.running = false;
+      clearInterval(this.timerRef);
+  }
+
+  clearTimer() {
+    this.running = false;
+    this.startText = 'Start';
+    this.counter = undefined;
+    clearInterval(this.timerRef);
+  }
+
+  ngOnDestroy() {
+    clearInterval(this.timerRef);
+  }
 
 }
